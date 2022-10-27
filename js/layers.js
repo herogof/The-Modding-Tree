83e0f898c1a5f93a615b843eee8aa4f006363782
herogof <116587861+herogof@users.noreply.github.com>
2022-10-27 20:55:54 +0200

    be

--- js/layers.js
+++ js/layers.js
@@ -27,10 +27,43 @@ addLayer("E", {
     upgrades: {
 11: {
 title: "My first steps",
-description: "Everithing starts with a small step",
+description: "Everything starts with a small step",
 cost: new Decimal(1),
 },
+12: {
+    title: "My first mine",
+    description: "Time to start mining",
+    cost: new Decimal(5),
+    },
     },
     layerShown(){return true}
 })
 
+addLayer("Mine", {
+    name: "Mine", // This is optional, only used in a few places, If absent it just uses the layer id.
+    symbol: "M", // This appears on the layer's node. Default is the id with the first letter capitalized
+    position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
+    startData() { return {
+        unlocked: true,
+		Ores: new Decimal(0),
+    }},
+    color: "#4BDC13",
+    requires: new Decimal(10), // Can be a function that takes requirement increases into account
+    resource: "ores", // Name of prestige currency
+    baseResource: "Exploration", // Name of resource prestige is based on
+    baseAmount() {return player.points}, // Get the current amount of baseResource
+    type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
+    exponent: 0.5, // Prestige currency exponent
+    gainMult() { // Calculate the multiplier for main currency from bonuses
+        mult = new Decimal(1)
+        return mult
+    },
+    gainExp() { // Calculate the exponent on main currency from bonuses
+        return new Decimal(1)
+    },
+    row: 0, // Row the layer is in on the tree (0 is the first row)
+    hotkeys: [
+        {key: "M", description: "M: Reset for ores", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
+    ],
+    layerShown(){return hasupgrade('E', 12)}
+})
